e5c7996945a22741d5a6b112a9c3f8a54bdc29a1
Nicolas TORO <nicolas.toro@epitech.eu>
2025-07-18 00:10:06 +0200

    FIX: change import settings to a better import alias "@"

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./styles.css";
+import {AuthProvider} from "@/contexts/authContext";
 
 
 
@@ -30,8 +31,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <AuthProvider>
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
