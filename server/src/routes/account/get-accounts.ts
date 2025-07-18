17751c2decb12d94dd7cdd275993ae116186bef8
Nicolas TORO <nicolas.toro@epitech.eu>
2025-07-19 00:59:06 +0200

    FEAT: implement a sort by role and email in get accounts

--- server/src/routes/account/get-accounts.ts
+++ server/src/routes/account/get-accounts.ts
@@ -8,7 +8,26 @@ const router = Router();
 
 router.get('/accounts', authenticateToken, authorizeAdmin, async (request: Request, response: Response): Promise<void> => {
     try {
-        const accounts = await Account.find({}, '-password');
+        //const accounts = await Account.find({}, '-password');
+        // sort accounts by role and email
+        const accounts = await Account.aggregate([
+            {
+                $addFields: {
+                    role_order: {
+                        $switch: {
+                            branches: [
+                                { case: { $eq: ["$role", "admin"] }, then: 0 },
+                                { case: { $eq: ["$role", "aer"] }, then: 1 },
+                                { case: { $eq: ["$role", "student"] }, then: 2 }
+                            ],
+                            default: 3
+                        }
+                    }
+                }
+            },
+            { $sort: { role_order: 1, email: 1 } },
+            { $project: { password: 0, role_order: 0 } }
+        ]);
         response.json(accounts);
     } catch (err) {
         response.status(500).json({ message: 'Server error.', details: err });
