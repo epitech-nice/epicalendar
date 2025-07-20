bc1cc1dc969064902c034d90f74e4c068c40eaa5
Nicolas TORO <nicolas.toro@epitech.eu>
2025-07-21 00:43:07 +0200

    ADD: add a profile service to use profile routes

---
+++ client/src/services/profileService.ts
@@ -0,0 +1,34 @@
+import api from "./api";
+import axios from "axios";
+import {Account, AccountUpdate} from "@/services/accountsService";
+
+
+
+export const ProfileService = {
+    async getProfile() : Promise<Account> {
+        try {
+            return (await api.get('/me')).data;
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                const message = error.response?.data?.message || "Profile retrieval error";
+                throw new Error(message);
+            } else {
+                throw new Error("Profile retrieval error");
+            }
+        }
+    },
+
+
+    async updateProfile(dataToUpdate: AccountUpdate): Promise<Account> {
+        try {
+            return (await api.put('/me', dataToUpdate)).data;
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                const message = error.response?.data?.message || "Profile update error";
+                throw new Error(message);
+            } else {
+                throw new Error("Profile update error");
+            }
+        }
+    }
+};
