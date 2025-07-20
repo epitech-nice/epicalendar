df9c417de3f4479c2618163462d85ed90f891026
Nicolas TORO <nicolas.toro@epitech.eu>
2025-07-21 00:48:51 +0200

    REFACTOR: refactor the display page to work with the new display account component

--- client/src/app/manage-accounts/display/[id]/page.tsx
+++ client/src/app/manage-accounts/display/[id]/page.tsx
@@ -5,6 +5,8 @@ import { useParams, useRouter } from 'next/navigation'
 import { AccountService, Account } from '@/services/accountsService'
 import { useAuth } from '@/contexts/authContext'
 import Link from "next/link";
+import Loading from "@/components/loading";
+import DisplayAccount from "@/components/displayAccount";
 
 
 
@@ -29,21 +31,6 @@ export default function ManageAccountsDisplayId() {
             setError(err instanceof Error ? err.message : 'An error occurred while fetching the account.')
         }
     }, [id])
-
-    const handleDeleteAccount = async (accountId: string) => {
-        console.log('Attempting to delete account with ID:', accountId);
-
-        if (!confirm('Are you sure you want to delete this account? This action cannot be undone.')) {
-            return;
-        }
-
-        try {
-            await AccountService.deleteAccount(accountId);
-            router.push('/manage-accounts');
-        } catch (err) {
-            setError(err instanceof Error ? err.message : 'An error occurred while deleting the account.');
-        }
-    };
 
 
 
@@ -72,12 +59,24 @@ export default function ManageAccountsDisplayId() {
 
 
 
-    if (loading) {// TODO: faire un composant de chargement
-        return <p>Loading...</p>;
-    }
+    let content = null;
 
-    if (error) {
-        return <p className="error">{error}</p>;
+    if (loading) {
+        content = (
+            <Loading/>
+        )
+
+    } else if (error) {
+        content = (
+            <div className="error">
+                {error}
+            </div>
+        )
+
+    } else {
+        content = account ? (
+            <DisplayAccount account={account}/>
+        ) : null
     }
 
 
@@ -88,69 +87,7 @@ export default function ManageAccountsDisplayId() {
                 Manage accounts - Display account
             </h1>
 
-            <div>
-                { /* Je peux pas faire de balise Image next parce que sa pu et qu'il faut autoriser le lien dans le next config */ }
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src={account?.photo || '/default-user.jpg'} alt="User Photo"/>
-            </div>
-
-            <div>
-                <button onClick={() => router.push(`/manage-accounts/edit/${account?._id}`)}>
-                    Edit
-                </button>
-
-                {account && (
-                    <button onClick={() => handleDeleteAccount(account._id!)}>
-                        Delete
-                    </button>
-                )}
-            </div>
-
-            <div>
-                 <em>
-                     <b>ID:</b>
-                     {account?._id}
-                 </em>
-
-                 <em>
-                     <b>Created at:</b>
-                     {account?.created_at ?
-                         new Date(account?.created_at).toLocaleDateString('en-US', {
-                         year: 'numeric',
-                         month: 'long',
-                         day: 'numeric'
-                     }) : 'unknown date' }
-                 </em>
-            </div>
-
-            <div>
-                {account?.first_name} {account?.last_name}
-            </div>
-
-            <div>
-                <a href={`mailto:${account?.email}`}>{account?.email}</a>
-            </div>
-
-            <div>
-                <b>Total guard time:</b>
-                {account?.guard_time
-                    ? `${Math.floor(account.guard_time / 60)} hours ${account.guard_time % 60} minutes`
-                    : 'No guard time recorded'}
-            </div>
-
-            <div>
-                <b>description</b>
-                {account?.description || 'No description provided.'}
-            </div>
-
-            <div>
-                <b>Preferred day:</b>
-                {account?.day || 'No preferred day set.'}
-
-
-                <b>Preferred room:</b>
-                {account?.room || 'No preferred room set.'}
-            </div>
+            {content}
 
             <Link href="/manage-accounts">
                 ← Back to accounts
